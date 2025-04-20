3a5926baaf4eb7907705529fdb31d40c55339779
DerickVT <57173605+Diederiki@users.noreply.github.com>
2025-04-20 21:50:08 +0200

    Update ubuntu-hardening.sh

--- ubuntu-hardening.sh
+++ ubuntu-hardening.sh
@@ -37,6 +37,10 @@ function update_system() {
 
 function change_ssh_port() {
     echo -e "${YELLOW}[*] Changing SSH port to 3022...${RESET}"
+    if ! command -v ufw &> /dev/null; then
+        echo -e "${YELLOW}[!] UFW not found. Installing ufw...${RESET}"
+        sudo apt update && sudo apt install -y ufw
+    fi
     sudo ufw allow 3022/tcp
     sudo sed -i 's/^#Port 22/Port 3022/;s/^Port 22/Port 3022/' /etc/ssh/sshd_config
     sudo systemctl restart sshd
